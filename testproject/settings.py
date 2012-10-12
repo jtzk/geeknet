b2a3bf523ba31e1c9d6d484ea76cbd4328ff19dd
abel <abel_tan@hotmail.com>
2012-10-12 12:51:14 +0800

    View surveys View survey -> All questions View survey -> Individual question Do survey -> All questions View survey results

--- testproject/settings.py
+++ testproject/settings.py
@@ -24,9 +24,9 @@ EMAIL_USE_TLS = False
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'testprojectdb',                      # Or path to database file if using sqlite3.
-        'USER': 'testprojectUser',                      # Not used with sqlite3.
-        'PASSWORD': '123',                  # Not used with sqlite3.
+        'NAME': 'surveydb',                      # Or path to database file if using sqlite3.
+        'USER': 'postgres',                      # Not used with sqlite3.
+        'PASSWORD': 'admin',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
