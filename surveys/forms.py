464fb40b5063bb7579f3c3493ce8dae753ea02ea
Jarryd <jarryd@yandao.org>
2012-10-17 02:17:13 +0800

    Results display -> Public, Users, Hidden

--- surveys/forms.py
+++ surveys/forms.py
@@ -6,7 +6,7 @@ from surveys.models import Survey, Question
 class SurveyCreationForm(forms.ModelForm):
     class Meta:
         model = Survey
-        exclude = {'status',}
+        exclude = {'status', 'owner'}
 
 class QuestionForm(forms.ModelForm):
     class Meta:
