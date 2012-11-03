4fa29e1c2625a9c0ff67ecd942956c9144b0294e
Jarryd <jarryd@yandao.org>
2012-11-04 00:29:57 +0800

    Unit tests

--- geeknet/settings.py
+++ geeknet/settings.py
@@ -19,8 +19,8 @@ EMAIL_USE_TLS = False
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'geeknet',                      # Or path to database file if using sqlite3.
-        'USER': 'postgres',                      # Not used with sqlite3.
+        'NAME': 'hahageek',                      # Or path to database file if using sqlite3.
+        'USER': 'geeknet',                      # Not used with sqlite3.
         'PASSWORD': '123',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
@@ -74,7 +74,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "C:/Documents and Settings/etch/Desktop/geeknet/static",
+    "C:/Documents and Settings/Jarryd/Desktop/geeknet/static",
 )
 
 # List of finder classes that know how to find static files in
@@ -114,7 +114,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "C:/Documents and Settings/etch/Desktop/geeknet/templates",
+    "C:/Documents and Settings/Jarryd/Desktop/geeknet/templates",
 )
 
 INSTALLED_APPS = (
--- geeknet/urls.py
+++ geeknet/urls.py
@@ -5,10 +5,10 @@ from geeknet.views import about, contact
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'surveys.views.index'),
     url(r'^surveys/', include('surveys.urls')),
     url(r'^accounts/', include('accounts.urls')),
     url(r'^about/', about, name='about'),
     url(r'^contact/', contact, name='contact'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'surveys.views.index'),
 )
